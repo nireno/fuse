75d6f1a675aeed30fee9e83e930177438255c814
Niren Orie <nireno@gmail.com>
2014-05-25 08:24:55 -0400

    Add GET request for Activities

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,9 +1,16 @@
 package controllers;
 
+import views.html.*;
+import models.*;
+
+import java.util.*;
+
 import play.*;
 import play.mvc.*;
+import play.libs.Json;
 
-import views.html.*;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 public class Application extends Controller {
 
@@ -11,4 +18,12 @@ public class Application extends Controller {
         return ok(index.render("Your new application is ready."));
     }
 
+  @BodyParser.Of(BodyParser.Json.class)
+  public static Result activity() {
+    ObjectNode result = Json.newObject();
+    List<Activity> activities = Activity.find.all();
+    result.put("status", "OK");
+    result.put("activities", Json.toJson(activities));
+    return ok(result);
+  }
 }
